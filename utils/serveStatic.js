a3c4f0845a03db2800c25c68b6fa78db1a7007ac
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-12-10 13:21:43 +0100

    serve content from server

--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -1,15 +1,25 @@
 import path from "node:path";
 import fs from "node:fs/promises";
 import { sendResponse } from "./sendResponse.js";
+import { getContentType } from "./getContentType.js";
 
 export async function serveStatic(req, res, directory) {
-  const filePath = path.join(directory, "public", "index.html");
+  const publicDir = path.join(directory, "public");
+  const filePath = path.join(
+    publicDir,
+    req.url === "/" ? "index.html" : req.url
+  );
+
+  const fileExtensions = path.extname(filePath);
+  const contentType = getContentType(fileExtensions);
+
+  // console.log(req.url);
+  console.log(fileExtensions);
 
   try {
     const content = await fs.readFile(filePath);
 
-    sendResponse(res, 200, "text/html", content);
-    console.log(content);
+    sendResponse(res, 200, contentType, content);
   } catch (err) {
     console.log(err);
   }
