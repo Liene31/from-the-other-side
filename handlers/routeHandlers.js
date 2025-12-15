9898ab52b129f4acd3b61d15eed1b5b3a674a429
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-12-15 22:49:16 +0100

    add post functionality

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -1,8 +1,15 @@
 import { getData } from "../utils/getData.js";
 import { sendResponse } from "../utils/sendResponse.js";
+import { parseJSONBody } from "../utils/parseJSONBody.js";
 
 export async function handleGet(res) {
-  const data = JSON.stringify(await getData());
-  sendResponse(res, 200, "application/json", data);
-  console.log(data);
+  const data = await getData();
+  const content = JSON.stringify(data);
+  sendResponse(res, 200, "application/json", content);
+  console.log(content);
+}
+
+export async function handlePost(req, res) {
+  const rawBody = await parseJSONBody(req);
+  console.log(rawBody);
 }
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import http from "node:http";
 import { serveStatic } from "./utils/serveStatic.js";
-import { handleGet } from "./handlers/routeHandlers.js";
+import { handleGet, handlePost } from "./handlers/routeHandlers.js";
 
 const PORT = 8000;
 
@@ -8,9 +8,12 @@ const __dirname = import.meta.dirname;
 // console.log("CWD", process.cwd()); --> prints cwd
 
 const server = http.createServer(async (req, res) => {
-  if (req.url.startsWith("/api") && req.method === "GET") {
-    console.log("api");
-    handleGet(res);
+  if (req.url === "/api") {
+    if (req.method === "GET") {
+      return await handleGet(res);
+    } else if (req.method === "POST") {
+      handlePost(req, res);
+    }
   } else if (!req.url.startsWith("/api")) {
     return await serveStatic(req, res, __dirname);
   }
