4edd03420bd2afbbcd0784e748256bc8a7901384
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-12-16 22:23:07 +0100

    add newSighting functionality

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -1,15 +1,20 @@
 import { getData } from "../utils/getData.js";
 import { sendResponse } from "../utils/sendResponse.js";
 import { parseJSONBody } from "../utils/parseJSONBody.js";
+import { addNewSighting } from "../utils/addNewSighting.js";
 
 export async function handleGet(res) {
   const data = await getData();
   const content = JSON.stringify(data);
   sendResponse(res, 200, "application/json", content);
-  console.log(content);
 }
 
 export async function handlePost(req, res) {
-  const rawBody = await parseJSONBody(req);
-  console.log(rawBody);
+  try {
+    const parsedBody = await parseJSONBody(req);
+    await addNewSighting(parsedBody);
+    sendResponse(res, 201, "application/json", JSON.stringify(parsedBody));
+  } catch (err) {
+    sendResponse(res, 400, "application/json", JSON.stringify({ error: err }));
+  }
 }
